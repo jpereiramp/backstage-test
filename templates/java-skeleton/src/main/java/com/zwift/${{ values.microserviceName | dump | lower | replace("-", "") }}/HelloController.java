9675c0c9126e08aaf86bd4ef593d56e61a303118
João Pereira <joao.pereira@zwift.com>
2025-05-13 21:36:28 -0300

    First commit

---
+++ templates/java-skeleton/src/main/java/com/zwift/${{ values.microserviceName | dump | lower | replace("-", "") }}/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.${{ values.microserviceName | dump | lower | replace("-", "") }};
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from ${{ values.microserviceName }}!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
